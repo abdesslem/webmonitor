a37290ca30da4d5bc520de305e23ea55845c9039
abdesslem <amriabdesslem@gmail.com>
2017-09-23 22:44:22 +0100

    fix typo

--- payments/lib/Payments/Controller/Plans.pm
+++ payments/lib/Payments/Controller/Plans.pm
@@ -1,13 +1,14 @@
 package Payments::Controller::Plans;
 use Mojo::Base 'Mojolicious::Controller';
+use LWP::UserAgent;
 
+my $planUrl = 'https://api.stripe.com/v1/plans';
 # This action will render a list of availables plans
 sub getAll {
 
   my $c = shift;
-  require LWP::UserAgent;
   my $ua = LWP::UserAgent->new;
-  my $req = HTTP::Request->new(GET => 'https://api.stripe.com/v1/plans?limit=4');
+  my $req = HTTP::Request->new(GET => $planUrl);
   $req->authorization_basic('sk_test_OkmvhmRZyPAUmYtjkrKbonka', '');
 
   $c->render(json => $ua->request($req)->content);
@@ -18,13 +19,11 @@ sub getAll {
 sub get {
 
   my $c = shift;
-  require LWP::UserAgent;
   my $ua = LWP::UserAgent->new;
-  my $req = HTTP::Request->new(GET => 'https://api.stripe.com/v1/plans?limit=4');
+  my $req = HTTP::Request->new(GET => $planUrl);
   $req->authorization_basic('sk_test_OkmvhmRZyPAUmYtjkrKbonka', '');
 
   $c->render(json => $ua->request($req)->content);
 
 }
-
 1;
